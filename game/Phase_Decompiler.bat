d5efd5f854d67150caca9586da542185a3b4190a
Brian Lach <brianlach72@gmail.com>
2015-08-08 22:59:57 -0400

    Ignoring .mf and .pyc files, removed .mf files from the repository. Compile them yourself when you clone the repository using Phase_Compiler.bat and type all.
    Former-commit-id: dc5e90cc80eb2bd7de160247c6c456a74efd18f9

--- game/Phase_Decompiler.bat
+++ game/Phase_Decompiler.bat
@@ -2,7 +2,7 @@
 title Cog Invasion Phase File Decompiler
 
 set /p file=What phase file do you want decompiled? 
-ppython.exe tools\tool_Multifile.py --filename %file% --mtype decompile
+..\Panda3D-CI\python\ppython.exe -B tool_Multifile.py --filename %file% --mtype decompile
 
 echo Done!
 pause >nul
