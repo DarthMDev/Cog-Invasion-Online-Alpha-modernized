09fe5a6e3a2a30496c43e5cc0e338663ee54f54b
Brian Lach <brianlach72@gmail.com>
2015-11-01 23:47:28 -0500

    Worked around a bug for Cogs not changing the hpr when they fly down.

--- game/lib/coginvasion/cog/DistributedSuit.py
+++ game/lib/coginvasion/cog/DistributedSuit.py
@@ -97,11 +97,12 @@ class DistributedSuit(Suit, DistributedAvatar, DistributedSmoothNode, DelayDelet
             self.moveIval.start(ts)
         self.animFSM.request('flyDown', [ts])
         yaw = random.uniform(0.0, 360.0)
-        self.setH(yaw)
+        self.getGeomNode().setH(yaw)
 
     def exitFlyingDown(self):
         self.stopMoving(finish = 1)
         self.animFSM.request('off')
+        self.getGeomNode().setH(0.0)
 
     def enterFlyingUp(self, startIndex, endIndex, ts = 0.0):
         if self.getHood() != '' and startIndex != -1 and endIndex != -1:
