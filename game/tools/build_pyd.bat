095782131c9a9c9fb49825d4950e6a9e07ef1d37
Brian Lach <brianlach72@gmail.com>
2015-11-07 12:52:01 -0500

    Forgot to update the pyd build tool.

--- game/tools/build_pyd.bat
+++ game/tools/build_pyd.bat
@@ -4,7 +4,7 @@ title Cog Invasion PYD Builder
 echo Building Cog Invasion Online lib folder into a pyd...
 ..\Panda3D-CI\python\ppython.exe -m direct.showutil.pfreeze -i direct.*.* -i lib.coginvasion.*.* -i encodings.* -i base64 -i site -o tools\libcoginvasion.pyd -x panda3d -x lib.server.*.* __main__
 echo Done building pyd!
-echo Packing/obfuscating the pyd file
-"C:\Users\Brian\Desktop\upx391w\upx" -9 tools\libcoginvasion.pyd
-echo All done!
+echo Protecting the pyd file...
+"C:\Program Files (x86)\The Enigma Protector\enigma32" ..\libcoginvasion.enigma
+echo Done!
 pause >nul
